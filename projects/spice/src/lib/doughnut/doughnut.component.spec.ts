6b73f00373aa50ee0b2c45c60949fdb6303d8645
Dan Osborne <110835608+danosborneDG@users.noreply.github.com>
2022-09-28 14:30:10 +0100

    test configuration

--- projects/spice/src/lib/doughnut/doughnut.component.spec.ts
+++ projects/spice/src/lib/doughnut/doughnut.component.spec.ts
@@ -1,23 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// import { DoughnutComponent } from './doughnut.component';
+
+// describe('DoughnutComponent', () => {
+//   let component: DoughnutComponent;
+//   let fixture: ComponentFixture<DoughnutComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ DoughnutComponent ]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(DoughnutComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+import { render, screen } from '@testing-library/angular';
 import { DoughnutComponent } from './doughnut.component';
 
-describe('DoughnutComponent', () => {
-  let component: DoughnutComponent;
-  let fixture: ComponentFixture<DoughnutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DoughnutComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(DoughnutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+describe('AppComponent', () => {
+  it('should render Welcome', async () => {
+    await render(DoughnutComponent);
+    await screen.getByText('doughnut works! god dammit');
   });
 });
